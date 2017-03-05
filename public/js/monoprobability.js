1ff73692f6b4ee734546aa3bc2c59c0950a5d33c
WarSame <graemecliffe@gmail.com>
2017-03-05 14:26:50 -0500

    Adding values in and separating them into a config file.

--- public/js/monoprobability.js
+++ public/js/monoprobability.js
@@ -1,1 +1,13 @@
 angular.module('monoprobabilityApp', [])
+.controller('OptimizationController', function(){
+  var squares = initializeSquares();
+  console.log(squares);
+})
+
+function initializeSquares(){
+  var numhouses = 0;
+  var railrent = 0;
+  var utilityrent = 0;
+
+  return squares;
+}
