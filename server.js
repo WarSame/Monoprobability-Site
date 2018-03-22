664fe3e68b78035f53f893210d9c1dfc32b83e11
WarSame <graemecliffe@gmail.com>
2018-03-21 20:47:39 -0400

    Pre- going serverless

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 // server.js
 
+const path = require("path")
 const port = 3000;
 const express = require("express");
+const cors = require("cors")
+const bodyParser = require("body-parser")
 const app = express();
 
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
-app.set("views", __dirname + "/views")
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
+
 app.locals.pretty = true
 
 app.get("/", function(req, res) {
-  res.render("index", {basedir:"."})
+  res.render("index")
 })
+
 
 app.listen(port, function(err) {
   if (err){
@@ -20,3 +28,5 @@ app.listen(port, function(err) {
 
   console.log("Server is listening on "+port)
 })
+
+//module.exports = app;
