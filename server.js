284202493649941b66598271b79a04136660aa06
WarSame <graemecliffe@gmail.com>
2018-03-25 15:51:54 -0400

    Working on local server. Works on AWS but might be dropping static link.

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const app = express();
 
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "pug")
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static("public"))
+app.set( "views", path.join( __dirname, "views" ) )
+app.set( "view engine", "pug" )
+app.use( cors() )
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded( { extended:true } ) )
+app.use( express.static( path.join( __dirname, "public" ) ) )
 
 app.locals.pretty = true
 
@@ -20,13 +20,4 @@ app.get("/", function(req, res) {
   res.render("index")
 })
 
-
-app.listen(port, function(err) {
-  if (err){
-    return console.log("Error: ", err)
-  }
-
-  console.log("Server is listening on "+port)
-})
-
-//module.exports = app;
+module.exports = app;
